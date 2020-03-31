cca9e58b603d6c82e6e72b66c3851431f186ef81
Deanout <dddehart@oakland.edu>
2020-03-31 03:34:56 -0400

    Remove cache to troubleshoot

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
-namespace :rake do
-  desc 'Run a task on a remote server.'
-  # run like: cap staging rake:invoke task=a_certain_task
-  task :invoke do
-    run("cd #{deploy_to}/current; /usr/bin/env rake #{ENV['seed.rb']} RAILS_ENV=#{rails_env}")
-  end
-end
+
 # config valid for current version and patch releases of Capistrano
 lock '~> 3.12.1'
 
