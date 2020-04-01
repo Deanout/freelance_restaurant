077edc3569e69383ce7241b60fe27bf0799b79a7
Deanout <dddehart@oakland.edu>
2020-04-01 03:47:41 -0400

    Add spaces maybe?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  
+  config.active_storage.service = :amazon
   # Show full error reports.
   config.consider_all_requests_local = true
 
