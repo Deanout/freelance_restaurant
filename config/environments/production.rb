ec7711c9d024e1cef2f1160d92b367734ca6c8fe
Deanout <dddehart@oakland.edu>
2020-04-01 04:22:51 -0400

    Changed production service from :aws -> :amazon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-  config.active_storage.service = :aws
+  config.active_storage.service = :amazon
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
